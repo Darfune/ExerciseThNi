91c84baaa1d54b4aa904996051dbada9b1064654
Darfune <harolep@gmail.com>
2023-11-26 16:39:52 +0200

    info dialog & regex

--- app/src/main/java/com/example/threenitasapp/ui/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/example/threenitasapp/ui/screens/login/LoginViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.threenitasapp.ui.screens.login
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.threenitasapp.common.Constants
 import com.example.threenitasapp.common.Resource
 import com.example.threenitasapp.domain.models.LoginBody
 import com.example.threenitasapp.domain.usecases.GetUserTokenUseCase
@@ -13,14 +17,50 @@ class LoginViewModel @Inject constructor(
     private val getUserTokenUseCase: GetUserTokenUseCase,
 ) : ViewModel() {
 
+    var isUserIdTextFieldDialogShown by mutableStateOf(false)
+        private set
+
+    fun onUserInfoIconClicked() {
+        isUserIdTextFieldDialogShown = true
+    }
+
+    fun onUserDismissTextFieldDialog() {
+        isUserIdTextFieldDialogShown = false
+    }
+
+    var isPassIdTextFieldDialogShown by mutableStateOf(false)
+        private set
+
+    fun onPassInfoIconClicked() {
+        isPassIdTextFieldDialogShown = true
+    }
+
+    fun onPassDismissTextFieldDialog() {
+        isPassIdTextFieldDialogShown = false
+    }
+
+
     suspend fun getToken(loginBody: LoginBody) =
         getUserTokenUseCase(loginBody)
             .collect { response ->
-            Log.d("ViewModel", "getToken: Called")
-            when (response){
-                is Resource.Success -> Log.d("ViewModel","getToken: ${response.data}")
-                is Resource.Error -> Log.d("ViewModel","getToken: ${response.message}")
-                is Resource.Loading -> Log.d("ViewModel","getToken: Loading")
+                Log.d("ViewModel", "getToken: Called")
+                when (response) {
+                    is Resource.Success -> Log.d("ViewModel", "getToken: ${response.data}")
+                    is Resource.Error -> Log.d("ViewModel", "getToken: ${response.message}")
+                    is Resource.Loading -> Log.d("ViewModel", "getToken: Loading")
+                }
             }
+
+    fun checkUserIDContent(textFieldFlag: Boolean, userInput: String): Boolean =
+        if (textFieldFlag) {
+            userInput.matches(Constants.userIDCondition)
+        } else {
+            userInput.matches(Constants.lengthCondition) &&
+                    userInput.matches(Constants.upperCaseLettersCondition) &&
+                    userInput.matches(Constants.lowerCaseLettersCondition) &&
+                    userInput.matches(Constants.digitsCondition) &&
+                    userInput.matches(Constants.specialCharacterCondition)
         }
+
+
 }
