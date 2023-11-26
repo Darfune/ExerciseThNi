28549d3dcd7a869bd75037f947ca39d92b8b5b11
Darfune <harolep@gmail.com>
2023-11-26 18:55:30 +0200

    login form validations tests

---
+++ app/src/test/java/com/example/threenitasapp/domain/usecases/client/PasswordValidationTest.kt
@@ -0,0 +1,61 @@
+package com.example.threenitasapp.domain.usecases.client
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class PasswordValidationTest {
+
+    private lateinit var validatePassword: PasswordValidation
+    @Before
+    fun setUp() {
+        validatePassword = PasswordValidation()
+    }
+
+    @Test
+    fun `Password is correct, validation succeeded`(){
+        val result = validatePassword("3NItas1!")
+        assert(result.successful)
+    }
+    @Test
+    fun`Password is incorrect, because it's blank`(){
+        val result = validatePassword("")
+        assertEquals(result.errorMessage, "The password can't be blank")
+    }
+
+    @Test
+    fun`Password is incorrect, length not 8`(){
+        val result1 = validatePassword("3tas1!")
+        assertEquals(result1.errorMessage, "The password must be 8 characters long")
+        val result2 = validatePassword("3NIatas1!")
+        assertEquals(result2.errorMessage, "The password must be 8 characters long")
+    }
+
+    @Test
+    fun`Password is incorrect, not 2 upper case letters`(){
+        val result = validatePassword("3nitas1!")
+        assertEquals(result.errorMessage, "The password must contain 2 Upper case letters")
+    }
+
+    @Test
+    fun`Password is incorrect, not 2 digits`(){
+        val result = validatePassword("3NItass!")
+        assertEquals(result.errorMessage, "The password must contain 2 digits")
+
+    }
+
+    @Test
+    fun`Password is incorrect, not 3 lower case letter`(){
+        val result = validatePassword("3NItaK4!")
+        assertEquals(result.errorMessage, "The password must contain 3 lower case letters")
+    }
+
+    @Test
+    fun`Password is incorrect, not 1 special character`(){
+        val result = validatePassword("3NItas4o")
+        assertEquals(result.errorMessage, "The password must contain 1 special character")
+    }
+
+
+}
---
+++ app/src/test/java/com/example/threenitasapp/domain/usecases/client/UserIdValidationTest.kt
@@ -0,0 +1,40 @@
+package com.example.threenitasapp.domain.usecases.client
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class UserIdValidationTest {
+
+    private lateinit var validateUserId: UserIdValidation
+    @Before
+    fun setUp() {
+        validateUserId = UserIdValidation()
+    }
+
+    @Test
+    fun `UserId is formatted correctly`(){
+        val result = validateUserId("TH1234")
+        assert(result.successful)
+    }
+
+    @Test
+    fun `UserId is formatted incorrectly, more than 4 numbers`(){
+        val result = validateUserId("TH12345")
+        assert(!result.successful)
+    }
+
+    @Test
+    fun `UserId is formatted incorrectly, doesn't begin with 4 upper case letters`(){
+        val result = validateUserId("T123H")
+        assert(!result.successful)
+    }
+
+    @Test
+    fun `UserId is formatted incorrectly, has more than 2 upper case letters`(){
+        val result = validateUserId("THV123")
+        assert(!result.successful)
+    }
+
+}
